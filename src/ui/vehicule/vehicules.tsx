302cb0f2c40bba3a2f7ab9d9144ada9f8f834cc7
Ben GREGORY <bengregorygbr@gmail.com>
2024-02-15 21:52:04 +0100

    added vehicules

--- src/ui/vehicule/vehicules.tsx
+++ src/ui/vehicule/vehicules.tsx
@@ -5,26 +5,45 @@ import { useQuery } from '@tanstack/react-query';
 import Filtre from './filtre';
 const Vehicules = () => {
 
-   
+
     const { isLoading, isError, data, error } = useQuery({
         queryKey: ['vehicules'],
         queryFn: getVehicules,
-      })
+    })
 
-      if (isLoading) {
+    if (isLoading) {
         return <div>Loading...</div>
-      }
+    }
 
-        if (isError) {
-            return <div>Error: </div>
-        }
+    if (isError) {
+        return <div>Error: </div>
+    }
+
+    const setFiltreParent = (filtre) => {
+        console.log(filtre);
+    }
+
+    if (isLoading) {
+        return <div className=' h-[80vh] flex flex-col items-center '>
+            <Filtre setFiltreParent={setFiltreParent} />
+
+            <div className="animate-bounce flex items-center gap-4 h-fit">
+                <svg id="logo-15" width="49" height="48" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M24.5 12.75C24.5 18.9632 19.4632 24 13.25 24H2V12.75C2 6.53679 7.03679 1.5 13.25 1.5C19.4632 1.5 24.5 6.53679 24.5 12.75Z" className="ccustom" fill="#17CF97"></path>
+                    <path d="M24.5 35.25C24.5 29.0368 29.5368 24 35.75 24H47V35.25C47 41.4632 41.9632 46.5 35.75 46.5C29.5368 46.5 24.5 41.4632 24.5 35.25Z" className="ccustom" fill="#17CF97"></path>
+                    <path d="M2 35.25C2 41.4632 7.03679 46.5 13.25 46.5H24.5V35.25C24.5 29.0368 19.4632 24 13.25 24C7.03679 24 2 29.0368 2 35.25Z" className="ccustom" fill="#17CF97"></path>
+                    <path d="M47 12.75C47 6.53679 41.9632 1.5 35.75 1.5H24.5V12.75C24.5 18.9632 29.5368 24 35.75 24C41.9632 24 47 18.9632 47 12.75Z" className="ccustom" fill="#17CF97"></path>
+                </svg>
+            </div>
+        </div>
+    }
 
 
     return (
         <div className=' h-[80vh] flex flex-col items-center'>
-            <Filtre />
-            <div className='grid grid-cols-5 gap-4 h-full'>
-                {data.map((vehicule:VehiculeType) => (
+            <Filtre setFiltreParent={setFiltreParent} />
+            <div className='lg:grid grid-cols-5 flex flex-col gap-4 h-full'>
+                {data.map((vehicule: VehiculeType) => (
                     <Vehicule key={vehicule.id} vehicule={vehicule} />
                 ))}
             </div>
