f777ac293f3d143a6ce59e4b57f687e6e6736288
Ben GREGORY <bengregorygbr@gmail.com>
2024-02-18 18:15:15 +0100

    calling backend

---
+++ src/api/login.ts
@@ -0,0 +1,17 @@
+const BACKEND_URL = 'http://localhost:8080';
+
+
+export const login = async (credentials:{login:string,password:string}) => {
+
+    const response = await fetch(`${BACKEND_URL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({login:credentials.login,password:credentials.password}),
+        redirect: 'follow'
+    });
+    
+    console.log(response);
+    return response.json();
+}
