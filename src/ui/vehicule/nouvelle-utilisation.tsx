85b2fbadcf315cf1db080511b2200a65221bc6e0
Ben GREGORY <bengregorygbr@gmail.com>
2024-02-18 18:16:51 +0100

    new form

--- src/ui/vehicule/nouvelle-utilisation.tsx
+++ src/ui/vehicule/nouvelle-utilisation.tsx
@@ -1,11 +1,187 @@
- 
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormField, FormLabel, FormControl, FormItem, FormDescription, FormMessage, } from "../../components/ui/form"
+import { Button } from "../../components/ui/button"
+import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from "../../components/ui/select"
+import { useQuery } from "@tanstack/react-query"
+import { getVehicules } from "../../api/queries"
+import { Vehicule } from "@/types/vehicule"
+import { toast } from "sonner"
+import { Calendar } from "../../components/ui/calendar"
+import { Popover, PopoverTrigger, PopoverContent } from "../../components/ui/popover"
+import { CalendarIcon } from "@radix-ui/react-icons"
+import cn from "classnames"
+
+
+
+const formSchema = z.object({
+    vehiculeId: z.string(),
+    date_debut_utilisation: z.date(),
+    date_find_utilisation: z.date()
+})
+
+
 const NouvelleUtilisation = () => {
+    const { isLoading, isError, data, error } = useQuery({
+        queryKey: ['vehicules'],
+        queryFn: getVehicules,
+    })
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+        toast.success("Utilisation enregistrée")
+
+    }
+
+    if (isError) {
+        return (
+            <p>{error.message}</p>
+        )
+    }
+
+    if (isLoading) {
+        return (
+            <p>LOADING</p>
+        )
+    }
+
     return (
-        <div className="text-3xl">
-                news
+        <div className="w-full flex justify-center">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="vehiculeId"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Véhicule</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Sélectionner un véhicule" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {
+                                            data.map((e: Vehicule, index: number) => {
+                                                return <SelectItem key={index} value={e.modele.modele}>{e.modele.marque} {" "} {e.modele.modele}</SelectItem>
+                                            })
+                                        }
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="date_debut_utilisation"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-col">
+                                <FormLabel>Date of birth</FormLabel>
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <FormControl>
+                                            <Button
+                                                variant={"outline"}
+                                                className={cn(
+                                                    "w-[240px] pl-3 text-left font-normal",
+                                                    !field.value && "text-muted-foreground"
+                                                )}
+                                            >
+                                                {field.value ? (
+                                                    format(field.value, "PPP")
+                                                ) : (
+                                                    <span>Pick a date</span>
+                                                )}
+                                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                            </Button>
+                                        </FormControl>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0" align="start">
+                                        <Calendar
+                                            mode="single"
+                                            selected={field.value}
+                                            onSelect={field.onChange}
+                                            disabled={(date) =>
+                                                date > new Date() || date < new Date("1900-01-01")
+                                            }
+                                            initialFocus
+                                        />
+                                    </PopoverContent>
+                                </Popover>
+                                <FormDescription>
+                                    Your date of birth is used to calculate your age.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="date_find_utilisation"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-col">
+                                <FormLabel>Date of birth</FormLabel>
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <FormControl>
+                                            <Button
+                                                variant={"outline"}
+                                                className={cn(
+                                                    "w-[240px] pl-3 text-left font-normal",
+                                                    !field.value && "text-muted-foreground"
+                                                )}
+                                            >
+                                                {field.value ? (
+                                                    format(field.value, "PPP")
+                                                ) : (
+                                                    <span>Pick a date</span>
+                                                )}
+                                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                            </Button>
+                                        </FormControl>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0" align="start">
+                                        <Calendar
+                                            mode="single"
+                                            selected={field.value}
+                                            onSelect={field.onChange}
+                                            disabled={(date) =>
+                                                date > new Date() || date < new Date("1900-01-01")
+                                            }
+                                            initialFocus
+                                        />
+                                    </PopoverContent>
+                                </Popover>
+                                <FormDescription>
+                                    Your date of birth is used to calculate your age.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Valider</Button>
+                </form>
+            </Form>
+
+
         </div>
     )
 }
 
 export default NouvelleUtilisation
-    
+
